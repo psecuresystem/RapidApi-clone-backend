818ad926204807c1f29fa9bfb1d59d4584154551
Chimexx <mezie.cjo@gmail.com>
2022-05-17 11:28:31 +0100

    Revert "Project Bootstrap"
    This reverts commit 7de85ca3c7ba83b62f2e83a83fde113a76420ef2.

--- src/common/constants/config.constant.ts
+++
@@ -1,9 +0,0 @@
-export const configConstant = {
-  database: {
-    host: 'DATABASE_HOST',
-    port: 'DATABASE_PORT',
-    username: 'DATABASE_USERNAME',
-    password: 'DATABASE_PASSWORD',
-    name: 'DATABASE_NAME',
-  }
-};
